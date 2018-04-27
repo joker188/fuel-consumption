5ffe4a03159597cf197045facdcd6fd34ba2ff52
joker188 <9891313+joker188@users.noreply.github.com>
2018-04-27 18:04:41 +0300

    add price

--- fuel.c
+++ fuel.c
@@ -2,14 +2,19 @@
 void main()
 {
 int DST,TANK,FC; // distance, tank capacity, fuel consumption 
+float PRICE,SUMP; // price, sum price
 
 printf ("Enter fuel tank capacity in liters: ");
-scanf("%d",&TANK);
+scanf ("%d",&TANK);
 printf ("Enter fuel consumption (km/l): ");
-scanf("%d",&FC);
+scanf ("%d",&FC);
+printf ("Enter price of fuel: ");
+scanf ("%f",&PRICE);
 
 DST=FC*TANK;
+SUMP=PRICE*TANK;
 
-printf("Your travel distance in km: %d\n",DST);
+printf ("Your travel distance in km: %d\n",DST);
+printf ("Money for fuel: %g\n",SUMP);
 
 }
